e7bcca1ff5b6c7b0f2d1ba7dfd0f0bf1f0f7ff37
Palethorn <wizzard405@yahoo.com>
2011-05-16 16:55:16 +0200

    Test commit

--- AVLTree/findNode.c
+++ AVLTree/findNode.c
@@ -6,11 +6,11 @@ node* findNode(node *subtree, int value, int *hops){
 	if(!subtree) return NULL;//Prazno [pod]stablo
 	if(subtree -> value == value) return subtree;//Ako je element pronadjen.
 	if(subtree -> value < value){
-		hops++;
+		(*hops)++;
 		return findNode(subtree -> right, value, hops);//Idi desno rekurzivnim pozivom.
 	}
 	if(subtree -> value > value){
-		hops++;
+		(*hops)++;
 		return findNode(subtree -> left, value, hops);//Idi lijevo rekurzivnim pozivom.
 	}
 }
--- AVLTree/main.c
+++ AVLTree/main.c
@@ -1,13 +1,18 @@
 #include "header.h"
 int main(){
 	node *tree = NULL;
-	addChild(&tree, 12);
-	addChild(&tree, 48);
-	addChild(&tree, 16);
-	addChild(&tree, -12);
-	addChild(&tree, 5);
+	node* element = NULL;
+	int hops = 0;
+	addChild(&tree, 3);
+	addChild(&tree, 2);
+	addChild(&tree, 1);
 	addChild(&tree, 4);
+	addChild(&tree, 5);
+	addChild(&tree, 6);
+	addChild(&tree, 7);
 	//removeChild(&tree, 12);
-	printNodes(tree);
+	element = findNode(tree,4,&hops);
+	printf("value: %d, balance factor: %d, left height: %d, right height: %d, hops: %d\n",element -> value, element -> balanceFactor, element -> lh, element -> rh, hops);
+	//printNodes(tree);
 	return 0;
 }
